633d156a94b427a2eb84199bc907db20148173d7
JMichu <michal0048@interia.pl>
2025-03-15 14:06:42 +0100

    Initial commit - project structure added

---
+++ Backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Server works!');
+});
+
+app.get('/api', (req, res) => {
+    res.json({ message: 'API works!' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`server works on http://localhost:${PORT}`);
+});
