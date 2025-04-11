c9c67337df3178c44ab884aa463c830eedaf30d4
JMichu <michal0048@interia.pl>
2025-04-11 18:53:09 +0200

    harmonize logo is now responsive

--- Frontend/src/pages/Login/Login.jsx
+++ Frontend/src/pages/Login/Login.jsx
@@ -1,8 +1,15 @@
+import React from 'react';
+import './Login.css';
+
 const Login = () => {
     return ( 
         <div className="login-page">
             <div className="login-container">
-                
+                <form action="/login" method="POST" className="login-form">
+                    <input type="email" name="email" placeholder="Email" required/>
+                    <input type="password" name="password" placeholder="password" required/>
+                    <button type="submit">Zaloguj</button>
+                </form>
             </div>
         </div>
     );
