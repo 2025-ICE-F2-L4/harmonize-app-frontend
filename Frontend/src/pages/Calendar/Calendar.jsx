850bf3553f3f4cfa123b3924a36e7660c9100c1c
JMichu <michal0048@interia.pl>
2025-04-14 00:59:50 +0200

    added some more activities (hardcoded)

--- Frontend/src/pages/Calendar/Calendar.jsx
+++ Frontend/src/pages/Calendar/Calendar.jsx
@@ -15,7 +15,12 @@ const Calendar = () => {
         headerToolbar={true}
         events={[
           { title: 'Camping', date: '2025-04-01T09:00:00', extendedProps: { people: ['Mom', 'Dad', 'Max', 'Ashley'] } },
-          { title: 'Horse riding', date: '2025-04-04T13:30:00', extendedProps: { people: ['Alice'] } }
+          { title: 'Horse riding', date: '2025-04-04T13:30:00', extendedProps: { people: ['Alice'] } },
+          { title: 'Picnic', date: '2025-04-15T13:30:00', extendedProps: { people: ['Alice','Mom','Dad'] } },
+          { title: 'Date', date: '2025-04-17T13:30:00', extendedProps: { people: ['Dad', 'Mom'] } },
+          { title: 'Cinema', date: '2025-04-20T20:30:00', extendedProps: { people: ['Max','Alice'] } },
+          { title: 'Bike trip', date: '2025-04-25T11:00:00', extendedProps: { people: ['Mom','Alice'] } },
+          { title: 'Board Games', date: '2025-04-30T18:30:00', extendedProps: { people: ['Alice','Mom','Dad','Max'] } },
         ]}
         editable={true}
         eventContent={ renderEventContent }
